5ef42b782ab3f883bea54c2fb3a71bcd89838447
Chris Cunningham <cjcunningham@hotmail.co.uk>
2020-10-05 16:06:23 +0100

    Implementation created for bubble sort

---
+++ BubbleSort/src/com/sparta/chris/Main.java
@@ -0,0 +1,9 @@
+package com.sparta.chris;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BubbleSort bs = new BubbleSort();
+        bs.print(bs.sort(new int[]{5,2,7,9,1,3}));
+    }
+}
