9a5c40967b6b753a8d8a9c14ae7dae262e7c45a9
Chris Cunningham <cjcunningham@hotmail.co.uk>
2020-10-21 09:49:23 +0100

    SortManager added

---
+++ OOPFourPillars/src/com/sparta/chris/AnimalInterface.java
@@ -0,0 +1,8 @@
+package com.sparta.chris;
+
+public interface Animal {
+    public abstract void move(); // Abstraction
+    public default void sound() {
+
+    }
+}
---
+++ SQLEmployeesProject/src/main/java/com/sparta/chris/Employee.java
@@ -0,0 +1,2 @@
+package com.sparta.chris;public class Employee {
+}
